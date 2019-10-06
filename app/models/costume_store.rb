275fd15c326c3cbd2d0dd0770d2d874b431ebaff
Felix <felixaviermartinez@gmail.com>
2019-10-06 17:48:35 -0400

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,8 @@
-# Create your CostumeStore class here
+class CostumeStore < ActiveRecord::Base
+end
+
+#     # def change
+#     #     create_table :costume_stores do |t|
+#     #         t.
+#     #     end
+#     # end
